604b2d36b0c811d2fd5ee71ba077d39b0924fb67
穆晨 <752572065@qq.com>
2017-06-26 12:35:54 +0800

    342

--- LIbProject/LIbProject/settings.py
+++ LIbProject/LIbProject/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.static',#一定要加上
             ],
         },
     },
@@ -125,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),#此处必须，为新添加
 )
